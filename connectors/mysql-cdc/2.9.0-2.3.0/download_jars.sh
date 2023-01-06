1f53d451b4b8ac5fe9ca9ca1be7a75160e4f0a6e
Vic Lau <xingyulliiuu@gmail.com>
2023-01-06 15:40:45 +0800

    Packaged Connector: mysql-cdc

---
+++ connectors/mysql-cdc/2.9.0-2.3.0/download_jars.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://repo1.maven.org/maven2/org/apache/flink/flink-connector-base/1.15.3/flink-connector-base-1.15.3.jar
+wget https://repo1.maven.org/maven2/com/ververica/flink-sql-connector-mysql-cdc/2.3.0/flink-sql-connector-mysql-cdc-2.3.0.jar
---
+++ connectors/mysql-cdc/README.md
@@ -0,0 +1,9 @@
+# vvp
+
+Custom images based on official VVP.
+
+# Packaged Connectors
+
+Name | Repository | Tags
+-|-|-
+mysql-cdc | registry.cn-beijing.aliyuncs.com/viclau/vvp-gateway-mysql-cdc | 2.8.1-2.3.0, 2.9.0-2.3.0
